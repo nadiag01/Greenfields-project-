dc7da8e8a95cd67442c34e86b6681c82909a8ca4
Nadia <nadiagainer@gmail.com>
2024-11-13 00:56:23 -0600

    feat: add tooltip to submit button

--- apre/apre-client/src/app/reports/sales/sales-by-region-tabular/sales-by-region-tabular.component.ts
+++ apre/apre-client/src/app/reports/sales/sales-by-region-tabular/sales-by-region-tabular.component.ts
@@ -21,7 +21,7 @@ import { TableComponent } from './../../../shared/table/table.component';
           </select>
         </div>
         <div class="form__actions">
-          <button class="button button--primary" type="submit">Submit</button>
+          <button class="button button--primary" title="click to fetch data" type="submit">Submit</button>
         </div>
       </form>
 
